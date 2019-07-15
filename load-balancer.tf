61cb45f84e864e9bb8a6c452c108917680950d5c
Emiliano Castagnari (a.k.a Torian) <ecastag@gmail.com>
2019-07-15 20:30:39 -0300

    Fix typo

--- load-balancer.tf
+++ load-balancer.tf
@@ -5,9 +5,9 @@ resource "aws_lb" "lb" {
   internal           = var.lb_internal
   load_balancer_type = var.lb_type
   subnets            = var.lb_subnets
-  security_groups    = if var_lb_type != "network" ? local.security_groups : null
   idle_timeout       = var.lb_idle_timeout
-  
+  security_groups    = var.lb_type != "network" ? local.security_groups : null
+
   enable_deletion_protection       = var.lb_enable_deletion_protection
   enable_cross_zone_load_balancing = var.lb_enable_cross_zone_load_balancing
   enable_http2                     = var.lb_enable_http2
